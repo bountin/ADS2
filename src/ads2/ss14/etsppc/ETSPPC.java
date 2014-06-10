20f8fdd7c7b17a91b436f94774d8ae322e4514d9
Martin Prebio <mp@25th-floor.com>
2014-06-11 00:34:44 +0200

    First implementation with just bounding and some simple optimizations/heuristics

--- src/ads2/ss14/etsppc/ETSPPC.java
+++ src/ads2/ss14/etsppc/ETSPPC.java
@@ -1,13 +1,17 @@
 package ads2.ss14.etsppc;
 
+import java.util.*;
+
 /**
  * Klasse zum Berechnen der Tour mittels Branch-and-Bound.
  * Hier sollen Sie Ihre L&ouml;sung implementieren.
  */
 public class ETSPPC extends AbstractETSPPC {
 
+	ETSPPCInstance instance;
+
 	public ETSPPC(ETSPPCInstance instance) {
-		// TODO: Hier ist der richtige Platz fuer Initialisierungen
+		this.instance = instance;
 	}
 
 	/**
@@ -22,7 +26,54 @@ public class ETSPPC extends AbstractETSPPC {
 	 */
 	@Override
 	public void run() {
-		// TODO: Diese Methode ist von Ihnen zu implementieren
+		search(new ArrayList<Location>());
 	}
 
+	private void search(final ArrayList<Location> visited) {
+		if (visited.size() == instance.getAllLocations().size()) {
+			setSolution(Main.calcObjectiveValue(visited), visited);
+			return;
+		}
+
+		ArrayList<Location> remainingLocations = new ArrayList<Location>();
+
+		// Filter already visited locations
+		for (Location l: instance.getAllLocations().values()) {
+			if (! visited.contains(l)) {
+				remainingLocations.add(l);
+			}
+		}
+
+		// Prune locations that are not reachable because of constraints
+		for(PrecedenceConstraint pc : instance.getConstraints()) {
+			if (! visited.contains(instance.getAllLocations().get(pc.getFirst()))) {
+				remainingLocations.remove(instance.getAllLocations().get(pc.getSecond()));
+			}
+		}
+
+		// Choose the Nearest Neighbour first
+		Collections.sort(remainingLocations, new Comparator<Location>() {
+			@Override
+			public int compare(Location l1, Location l2) {
+				if (visited.size() == 0) return 0;
+
+				Double l1Cost = visited.get(visited.size() - 1).distanceTo(l1);
+				Double l2Cost = visited.get(visited.size() - 1).distanceTo(l2);
+
+				return l1Cost.compareTo(l2Cost);
+			}
+		});
+
+		for (Location nextLocation: remainingLocations) {
+			ArrayList<Location> newVisited = new ArrayList<Location>(visited.size());
+			for (Location l: visited) newVisited.add(l);
+
+			newVisited.add(nextLocation);
+
+			BnBSolution bestSolution = getBestSolution();
+			if (bestSolution == null || bestSolution.getUpperBound() > Main.calcObjectiveValue(newVisited)) {
+				search(newVisited);
+			}
+		}
+	}
 }
